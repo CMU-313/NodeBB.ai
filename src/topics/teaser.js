7f849a9f66f2ff8c26cec993a410248d93440403
BigBoa19 <121171339+BigBoa19@users.noreply.github.com>
2025-09-30 14:18:44 +0000

    AI Agent changes to fix code smell src/topics/teaster Function with many returns (count = 14): exports

--- src/topics/teaser.js
+++ src/topics/teaser.js
@@ -10,8 +10,66 @@ const posts = require('../posts');
 const plugins = require('../plugins');
 const utils = require('../utils');
 
-module.exports = function (Topics) {
-	Topics.getTeasers = async function (topics, options) {
+// Pure helper kept at module scope to avoid inflating return count in the export wrapper
+function calcTeaserIndex(teaserPost, postCountInTopic, sortNewToOld) {
+	if (teaserPost === 'first') {
+		return 1;
+	}
+
+	if (sortNewToOld) {
+		return Math.min(2, postCountInTopic);
+	}
+	return postCountInTopic;
+}
+
+async function getPreviousNonBlockedPost(postData, blockedUids, deps) {
+	const { Topics } = deps;
+	let isBlocked = false;
+	let prevPost = postData;
+	const postsPerIteration = 5;
+	let start = 0;
+	let stop = start + postsPerIteration - 1;
+	let checkedAllReplies = false;
+
+	function checkBlocked(post) {
+		const isPostBlocked = blockedUids.includes(parseInt(post.uid, 10));
+		prevPost = !isPostBlocked ? post : prevPost;
+		return isPostBlocked;
+	}
+
+	do {
+		/* eslint-disable no-await-in-loop */
+		let pids = await db.getSortedSetRevRange(`tid:${postData.tid}:posts`, start, stop);
+		if (!pids.length) {
+			checkedAllReplies = true;
+			const mainPid = await Topics.getTopicField(postData.tid, 'mainPid');
+			pids = [mainPid];
+		}
+		const prevPosts = await posts.getPostsFields(pids, ['pid', 'uid', 'timestamp', 'tid', 'content']);
+		isBlocked = prevPosts.every(checkBlocked);
+		start += postsPerIteration;
+		stop = start + postsPerIteration - 1;
+	} while (isBlocked && prevPost && prevPost.pid && !checkedAllReplies);
+
+	return prevPost;
+}
+
+async function handleBlocks(uid, teasers, deps) {
+	const blockedUids = await user.blocks.list(uid);
+	if (!blockedUids.length) {
+		return teasers;
+	}
+
+	return Promise.all(teasers.map(async (postData) => {
+		if (blockedUids.includes(parseInt(postData.uid, 10))) {
+			return getPreviousNonBlockedPost(postData, blockedUids, deps);
+		}
+		return postData;
+	}));
+}
+
+function createGetTeasers(deps) {
+	return async function getTeasers(topics, options) {
 		if (!Array.isArray(topics) || !topics.length) {
 			return [];
 		}
@@ -47,20 +105,20 @@ module.exports = function (Topics) {
 			user.getSettings(uid),
 		]);
 		let postData = allPostData.filter(post => post && post.pid);
-		postData = await handleBlocks(uid, postData);
+		postData = await handleBlocks(uid, postData, deps);
 		postData = postData.filter(Boolean);
 		const uids = _.uniq(postData.map(post => post.uid));
 		const sortNewToOld = callerSettings.topicPostSort === 'newest_to_oldest';
 		const usersData = await user.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture']);
 
 		const users = {};
-		usersData.forEach((user) => {
-			users[user.uid] = user;
+		usersData.forEach((u) => {
+			users[u.uid] = u;
 		});
 		postData.forEach((post) => {
 			// If the post author isn't represented in the retrieved users' data,
 			// then it means they were deleted, assume guest.
-			if (!users.hasOwnProperty(post.uid)) {
+			if (!Object.prototype.hasOwnProperty.call(users, post.uid)) {
 				post.uid = 0;
 			}
 
@@ -83,77 +141,29 @@ module.exports = function (Topics) {
 		const result = await plugins.hooks.fire('filter:teasers.get', { teasers: teasers, uid: uid });
 		return result.teasers;
 	};
+}
 
-	function calcTeaserIndex(teaserPost, postCountInTopic, sortNewToOld) {
-		if (teaserPost === 'first') {
-			return 1;
-		}
-
-		if (sortNewToOld) {
-			return Math.min(2, postCountInTopic);
-		}
-		return postCountInTopic;
-	}
-
-	async function handleBlocks(uid, teasers) {
-		const blockedUids = await user.blocks.list(uid);
-		if (!blockedUids.length) {
-			return teasers;
-		}
-
-		return await Promise.all(teasers.map(async (postData) => {
-			if (blockedUids.includes(parseInt(postData.uid, 10))) {
-				return await getPreviousNonBlockedPost(postData, blockedUids);
-			}
-			return postData;
-		}));
-	}
-
-	async function getPreviousNonBlockedPost(postData, blockedUids) {
-		let isBlocked = false;
-		let prevPost = postData;
-		const postsPerIteration = 5;
-		let start = 0;
-		let stop = start + postsPerIteration - 1;
-		let checkedAllReplies = false;
-
-		function checkBlocked(post) {
-			const isPostBlocked = blockedUids.includes(parseInt(post.uid, 10));
-			prevPost = !isPostBlocked ? post : prevPost;
-			return isPostBlocked;
-		}
-
-		do {
-			/* eslint-disable no-await-in-loop */
-			let pids = await db.getSortedSetRevRange(`tid:${postData.tid}:posts`, start, stop);
-			if (!pids.length) {
-				checkedAllReplies = true;
-				const mainPid = await Topics.getTopicField(postData.tid, 'mainPid');
-				pids = [mainPid];
-			}
-			const prevPosts = await posts.getPostsFields(pids, ['pid', 'uid', 'timestamp', 'tid', 'content']);
-			isBlocked = prevPosts.every(checkBlocked);
-			start += postsPerIteration;
-			stop = start + postsPerIteration - 1;
-		} while (isBlocked && prevPost && prevPost.pid && !checkedAllReplies);
-
-		return prevPost;
-	}
-
-	Topics.getTeasersByTids = async function (tids, uid) {
+function createGetTeasersByTids(deps) {
+	const { Topics } = deps;
+	return async function getTeasersByTids(tids, uid) {
 		if (!Array.isArray(tids) || !tids.length) {
 			return [];
 		}
 		const topics = await Topics.getTopicsFields(tids, ['tid', 'postcount', 'teaserPid', 'mainPid']);
-		return await Topics.getTeasers(topics, uid);
+		return Topics.getTeasers(topics, uid);
 	};
+}
 
-	Topics.getTeaser = async function (tid, uid) {
-		const teasers = await Topics.getTeasersByTids([tid], uid);
+function createGetTeaser() {
+	return async function getTeaser(tid, uid) {
+		const teasers = await this.getTeasersByTids([tid], uid);
 		return Array.isArray(teasers) && teasers.length ? teasers[0] : null;
 	};
+}
 
-	Topics.updateTeaser = async function (tid) {
+function createUpdateTeaser(deps) {
+	const { Topics } = deps;
+	return async function updateTeaser(tid) {
 		let pid = await Topics.getLatestUndeletedReply(tid);
 		pid = pid || null;
 		if (pid) {
@@ -162,4 +172,13 @@ module.exports = function (Topics) {
 			await Topics.deleteTopicField(tid, 'teaserPid');
 		}
 	};
+}
+
+module.exports = function (Topics) {
+	const deps = { Topics };
+	// Bind factories to Topics to avoid returns in this wrapper function
+	Topics.getTeasers = createGetTeasers(deps);
+	Topics.getTeasersByTids = createGetTeasersByTids(deps);
+	Topics.getTeaser = createGetTeaser();
+	Topics.updateTeaser = createUpdateTeaser(deps);
 };
