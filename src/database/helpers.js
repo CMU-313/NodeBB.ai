f0c72ae0c6211eafa7c7e0600b9481a74b4e5ad4
Nikash-B <88526814+Nikash-B@users.noreply.github.com>
2026-03-12 14:20:36 +0000

    Refactered src/database/helpers.js

--- src/database/helpers.js
+++ src/database/helpers.js
@@ -2,27 +2,54 @@
 
 const helpers = module.exports;
 
-helpers.mergeBatch = function (batchData, start, stop, sort) {
-	function getFirst() {
-		let selectedArray = batchData[0];
-		for (let i = 1; i < batchData.length; i++) {
-			if (batchData[i].length && (
-				!selectedArray.length ||
-				(sort === 1 && batchData[i][0].score < selectedArray[0].score) ||
-				(sort === -1 && batchData[i][0].score > selectedArray[0].score)
-			)) {
-				selectedArray = batchData[i];
-			}
-		}
-		return selectedArray.length ? selectedArray.shift() : null;
+function isBetterCandidate(candidate, selectedArray, sort) {
+	if (!candidate.length) {
+		return false;
 	}
+	if (!selectedArray.length) {
+		return true;
+	}
+	if (sort === 1) {
+		return candidate[0].score < selectedArray[0].score;
+	}
+	if (sort === -1) {
+		return candidate[0].score > selectedArray[0].score;
+	}
+	return false;
+}
+
+function getFirst(batchData, sort) {
+	let selectedArray = batchData[0];
+
+	for (let i = 1; i < batchData.length; i++) {
+		if (isBetterCandidate(batchData[i], selectedArray, sort)) {
+			selectedArray = batchData[i];
+		}
+	}
+
+	return selectedArray.length ? selectedArray.shift() : null;
+}
+
+function shouldContinue(item, resultLength, start, stop) {
+	if (!item) {
+		return false;
+	}
+	if (stop === -1) {
+		return true;
+	}
+	return resultLength < (stop - start + 1);
+}
+
+helpers.mergeBatch = function (batchData, start, stop, sort) {
 	let item = null;
 	const result = [];
+
 	do {
-		item = getFirst(batchData);
+		item = getFirst(batchData, sort);
 		if (item) {
 			result.push(item);
 		}
-	} while (item && (result.length < (stop - start + 1) || stop === -1));
+	} while (shouldContinue(item, result.length, start, stop));
+
 	return result;
 };
