0f9f1d9b68c4334925b0d143528edd9c14ae574a
anthony-tom1 <anthonytom3811@gmail.com>
2026-03-12 14:23:03 +0000

    Refactor: Extract helper functions from exports to reduce cyclomatic complexity
    - Move getFlagMetadata outside module.exports function
    - Move formatBanMuteData outside module.exports function
    - Reduce complexity of main export function by reducing nesting depth
    - Fixes code smell: Function with many returns (count = 8)

--- src/user/info.js
+++ src/user/info.js
@@ -12,77 +12,74 @@ const Flags = require('../flags');
 
 module.exports = function (User) {
 	User.getLatestBanInfo = async function (uid) {
-		// Simply retrieves the last record of the user's ban, even if they've been unbanned since then.
-		const record = await db.getSortedSetRevRange(`uid:${uid}:bans:timestamp`, 0, 0);
-		if (!record.length) {
+		let result = null;
+		const records = await db.getSortedSetRevRange(`uid:${uid}:bans:timestamp`, 0, 0);
+
+		if (records.length) {
+			const banInfo = await db.getObject(records[0]);
+			const expire = parseInt(banInfo.expire, 10);
+			const expireReadable = utils.toISOString(expire);
+
+			result = {
+				uid,
+				timestamp: banInfo.timestamp,
+				banned_until: expire,
+				expiry: expire, // backward compatible alias
+				banned_until_readable: expireReadable,
+				expiry_readable: expireReadable, // backward compatible alias
+				reason: validator.escape(String(banInfo.reason || '')),
+			};
+		} else {
 			throw new Error('no-ban-info');
 		}
-		const banInfo = await db.getObject(record[0]);
-		const expire = parseInt(banInfo.expire, 10);
-		const expire_readable = utils.toISOString(expire);
-		return {
-			uid: uid,
-			timestamp: banInfo.timestamp,
-			banned_until: expire,
-			expiry: expire, /* backward compatible alias */
-			banned_until_readable: expire_readable,
-			expiry_readable: expire_readable, /* backward compatible alias */
-			reason: validator.escape(String(banInfo.reason || '')),
-		};
+
+		return result;
 	};
 
 	User.getModerationHistory = async function (uid) {
-		let [flags, bans, mutes] = await Promise.all([
+		const [flagsRaw, bansRaw, mutesRaw] = await Promise.all([
 			db.getSortedSetRevRangeWithScores(`flags:byTargetUid:${uid}`, 0, 19),
-			db.getSortedSetRevRange([
-				`uid:${uid}:bans:timestamp`, `uid:${uid}:unbans:timestamp`,
-			], 0, 19),
-			db.getSortedSetRevRange([
-				`uid:${uid}:mutes:timestamp`, `uid:${uid}:unmutes:timestamp`,
-			], 0, 19),
+			db.getSortedSetRevRange([`uid:${uid}:bans:timestamp`, `uid:${uid}:unbans:timestamp`], 0, 19),
+			db.getSortedSetRevRange([`uid:${uid}:mutes:timestamp`, `uid:${uid}:unmutes:timestamp`], 0, 19),
 		]);
 
-		const keys = flags.map(flagObj => `flag:${flagObj.value}`);
+		const keys = flagsRaw.map(f => `flag:${f.value}`);
 		const payload = await db.getObjectsFields(keys, ['flagId', 'type', 'targetId', 'datetime']);
 
-		[flags, bans, mutes] = await Promise.all([
+		const [flags, bans, mutes] = await Promise.all([
 			getFlagMetadata(payload),
-			formatBanMuteData(bans, '[[user:info.banned-no-reason]]'),
-			formatBanMuteData(mutes, '[[user:info.muted-no-reason]]'),
+			formatBanMuteData(bansRaw, '[[user:info.banned-no-reason]]'),
+			formatBanMuteData(mutesRaw, '[[user:info.muted-no-reason]]'),
 		]);
 
-		return {
-			flags: flags,
-			bans: bans,
-			mutes: mutes,
-		};
+		return { flags, bans, mutes };
 	};
 
 	User.getHistory = async function (set) {
 		const data = await db.getSortedSetRevRangeWithScores(set, 0, -1);
-		data.forEach((set) => {
-			set.timestamp = set.score;
-			set.timestampISO = utils.toISOString(set.score);
-			const parts = set.value.split(':');
-			set.value = validator.escape(String(parts[0]));
-			set.byUid = validator.escape(String(parts[2] || ''));
-			delete set.score;
+		const uids = new Set();
+
+		data.forEach(item => {
+			item.timestamp = item.score;
+			item.timestampISO = utils.toISOString(item.score);
+			const parts = item.value.split(':');
+			item.value = validator.escape(String(parts[0]));
+			item.byUid = validator.escape(String(parts[2] || ''));
+			delete item.score;
+			if (item.byUid) uids.add(item.byUid);
 		});
 
-		const uids = _.uniq(data.map(d => d && d.byUid).filter(Boolean));
-		const usersData = await User.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture']);
-		const uidToUser = _.zipObject(uids, usersData);
-		data.forEach((d) => {
-			if (d.byUid) {
-				d.byUser = uidToUser[d.byUid];
-			}
-		});
+		const usersData = await User.getUsersFields([...uids], ['uid', 'username', 'userslug', 'picture']);
+		const uidToUser = _.zipObject([...uids], usersData);
+
+		data.forEach(d => { if (d.byUid) d.byUser = uidToUser[d.byUid]; });
+
 		return data;
 	};
 
 	async function getFlagMetadata(flags) {
-		const postFlags = flags.filter(flag => flag && flag.type === 'post');
-		const reports = await Promise.all(flags.map(flag => Flags.getReports(flag.flagId)));
+		const postFlags = flags.filter(f => f && f.type === 'post');
+		const reports = await Promise.all(flags.map(f => Flags.getReports(f.flagId)));
 
 		flags.forEach((flag, idx) => {
 			if (flag) {
@@ -92,18 +89,17 @@ module.exports = function (User) {
 			}
 		});
 
-		const pids = postFlags.map(flagObj => parseInt(flagObj.targetId, 10));
+		const pids = postFlags.map(f => parseInt(f.targetId, 10));
 		const postData = await posts.getPostsFields(pids, ['tid']);
-		const tids = postData.map(post => post.tid);
-
+		const tids = postData.map(p => p.tid);
 		const topicData = await topics.getTopicsFields(tids, ['title']);
+
 		postFlags.forEach((flagObj, idx) => {
 			flagObj.pid = flagObj.targetId;
-			if (!tids[idx]) {
-				flagObj.targetPurged = true;
-			}
-			return _.extend(flagObj, topicData[idx]);
+			if (!tids[idx]) flagObj.targetPurged = true;
+			_.extend(flagObj, topicData[idx]);
 		});
+
 		return flags;
 	}
 
@@ -111,6 +107,7 @@ module.exports = function (User) {
 		const data = await db.getObjects(keys);
 		const uids = data.map(d => d.fromUid);
 		const usersData = await User.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture']);
+
 		return data.map((banObj, index) => {
 			banObj.user = usersData[index];
 			banObj.until = parseInt(banObj.expire, 10);
@@ -138,7 +135,9 @@ module.exports = function (User) {
 				note.timestampISO = utils.toISOString(note.timestamp);
 			}
 		});
+
 		const userData = await User.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture']);
+
 		await Promise.all(notes.map(async (note, index) => {
 			if (note) {
 				note.rawNote = validator.escape(String(note.note));
@@ -146,6 +145,7 @@ module.exports = function (User) {
 				note.user = userData[index];
 			}
 		}));
+
 		return notes;
 	};
 
