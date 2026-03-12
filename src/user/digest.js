797a7b8ae9c27f777bd7c328ae76237d947ee9f6
PhilipD8 <philipda@andrew.cmu.edu>
2026-03-12 14:32:02 +0000

    Refactor complex binary expression in digest.js

--- src/user/digest.js
+++ src/user/digest.js
@@ -108,10 +108,11 @@ Digest.send = async function (data) {
 				getTermTopics(data.interval, userObj.uid),
 			]);
 			const unreadNotifs = notifications.filter(Boolean);
+			const hasUnreadNotifs = unreadNotifs.length > 0;
+			const hasNewTopics = [topics.top, topics.popular, topics.recent].some(topicList => topicList.length > 0);
+			const hasUnreadChats = publicRooms.length > 0;
 			// If there are no notifications and no new topics and no unread chats, don't bother sending a digest
-			if (!unreadNotifs.length &&
-				!topics.top.length && !topics.popular.length && !topics.recent.length &&
-				!publicRooms.length) {
+			if (!hasUnreadNotifs && !hasNewTopics && !hasUnreadChats) {
 				return;
 			}
 
