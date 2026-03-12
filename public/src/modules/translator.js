562a28be08e097e55298cdb0ef67ff0a7c0ac5e4
Daniel Rodriguez <dalejandro111803@gmail.com>
2026-03-12 15:52:49 +0000

    refactor: simplify loadClient function by extracting handleData

--- public/src/modules/translator.js
+++ public/src/modules/translator.js
@@ -3,23 +3,33 @@
 const factory = require('./translator.common');
 
 define('translator', ['jquery', 'utils'], function (jQuery, utils) {
-	function loadClient(language, namespace) {
-		return new Promise(function (resolve, reject) {
-			jQuery.getJSON([config.asset_base_url, 'language', language, namespace].join('/') + '.json?' + config['cache-buster'], function (data) {
-				const payload = {
-					language: language,
-					namespace: namespace,
-					data: data,
-				};
-				require(['hooks'], function (hooks) {
-					hooks.fire('action:translator.loadClient', payload);
-					resolve(payload.promise ? Promise.resolve(payload.promise) : data);
-				});
-			}).fail(function (jqxhr, textStatus, error) {
-				reject(new Error(textStatus + ', ' + error));
-			});
+	function handleData(data, language, namespace, resolve) {
+		require(['hooks'], function (hooks) {
+			const payload = {
+				language: language,
+				namespace: namespace,
+				data: data,
+			};
+			hooks.fire('action:translator.loadClient', payload);
+			resolve(payload.promise ? Promise.resolve(payload.promise) : data);
 		});
 	}
+
+	function loadClient(language, namespace) {
+		const url = [config.asset_base_url, 'language', language, namespace].join('/') + '.json?' + config['cache-buster'];
+
+		return new Promise(function (resolve, reject) {
+			jQuery.getJSON(url)
+				.done(function (data) {
+					handleData(data, language, namespace, resolve);
+				})
+				.fail(function (jqxhr, textStatus, error) {
+					reject(new Error(textStatus + ', ' + error));
+				});
+		});
+	}
+
 	const warn = function () { console.warn.apply(console, arguments); };
+
 	return factory(utils, loadClient, warn);
 });
