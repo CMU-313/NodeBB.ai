e8e1ed30e73de8fcdab0eee3ffc8b11dbde55fcd
Phasakorn Chivatxaranukul <61149119+pskodr@users.noreply.github.com>
2026-03-12 14:35:03 +0000

    task-1c, after review changes

--- src/posts/topics.js
+++ src/posts/topics.js
@@ -6,7 +6,7 @@ const user = require('../user');
 const utils = require('../utils');
 
 module.exports = function (Posts) {
-	Posts.getPostsFromSet = async function ({ set, start, stop, uid, reverse }) {
+	Posts.getPostsFromSet = async function ({ set, start, stop, uid, reverse = false }) {
 		const pids = await Posts.getPidsFromSet(set, start, stop, reverse);
 		const posts = await Posts.getPostsByPids(pids, uid);
 		return await user.blocks.filter(uid, posts);
@@ -44,8 +44,8 @@ module.exports = function (Posts) {
 			const postIndex = utils.isNumber(indices[index]) ? parseInt(indices[index], 10) + 1 : null;
 
 			if (slug && postIndex) {
-				const index = postIndex === 1 ? '' : `/${postIndex}`;
-				return `/topic/${slug}${index}`;
+				const postIndexSuffix = postIndex === 1 ? '' : `/${postIndex}`;
+				return `/topic/${slug}${postIndexSuffix}`;
 			}
 			return null;
 		});
